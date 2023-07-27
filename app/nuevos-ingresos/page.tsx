c0bfc77b9b36188dfb8073f7cf32703e088545e4
LauMFlores <mlaumflores@gmail.com>
2023-07-26 22:21:35 -0300

    login: estilos pantalla chica

--- app/nuevos-ingresos/page.tsx
+++ app/nuevos-ingresos/page.tsx
@@ -5,12 +5,29 @@ import Layout from "../layout2";
   export default function nuevosIngresos (){
 
     const pageTitle = 'Nuevos Ingresos';
-      return (
-          <Layout pageTitle={pageTitle}>
-              <h1>Nuevos Ingresos de Primer Año</h1>
-              
-          </Layout>
-      
-      )
+    return (
+      <Layout pageTitle={pageTitle}>
+          <main className="campus"> 
+          <h1> BIENVENIDO! </h1>
+          <div style={{backgroundColor: 'white'}}>
+          <div style={{backgroundColor: 'var(--verde1)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--verde2)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--verde3)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--verde4)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--verde5)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--verde6)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--verde7)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--gris1)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--gris2)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--gris3)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--gris4)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--gris5)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--gris6-transparente-menu)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--negro)'}}> COLOR</div>
+          <div style={{backgroundColor: 'var(--blanco)'}}> COLOR</div>
+          </div>
+          </main>
+      </Layout>
+  )
   }
 
