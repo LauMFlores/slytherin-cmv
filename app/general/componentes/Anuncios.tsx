8eca0a69e1afe17e9c7adf14b709a5d266601c40
LauMFlores <mlaumflores@gmail.com>
2023-07-28 17:16:04 -0300

    general: anuncios

--- app/general/componentes/Anuncios.tsx
+++ app/general/componentes/Anuncios.tsx
@@ -1,8 +1,13 @@
 // import data from "../data/data_anuncios.json";
 import '../estilos/anuncios.css';
+import data from '../../data_usuarios.json'
 
 export default function Anuncios () {
-
+    const prefectos = data.filter(user => user.prefecto === true);
+    const jefe = data.find(user => user.jefe === true);
+    const cazadores = data.filter(user => user.quidditch === "cazador");
+    const bateadores= data.filter(user => user.quidditch === "bateador")
+    const buscador= data.find(user => user.quidditch === "buscador");
     return (
         <div>
             <h3> Tablón de Anuncios</h3>
@@ -11,14 +16,33 @@ export default function Anuncios () {
                 <h4>Prefectos y Jefe de Slytherin</h4>
                 <p>Con orgullo anunciamos a los prefectos designados para el año escolar. Los felicitamos por el logro!!!</p>
                 <ul>
-                    <li>Prefecto 1</li>
-                    <li>Prefecto 2</li>
+                    {prefectos.map(user => (
+                        <li key={user.username}>{user.nombre} {user.apellido}</li>
+                    ))}
                 </ul>
                 <p>Por 5to año consecutivo, con la sabiduria y rigurosidad de siempre, anunciamos tambien a nuestro jefe de casa:</p>
-                <p>Prof. Slughorn.</p>
+                {jefe && <p>Prof. {jefe.nombre} {jefe.apellido}</p>}
                 <p>Por inquitudes, problemas o cualquier necesidad, tratara los temas relacionados a la Casa Slytherin en su despacho en el aula de pociones, de Jueves a Sabados de 15 a 19hs.</p>
             </div>
             <h4>Equipo de Quidditch</h4>
+            <p> Anunciamos las posiciones de quidditch definidas para este año:</p>
+            <p> Cazadores: 
+            <ul>
+                    {cazadores.map(user => (
+                        <li key={user.username}>{user.nombre} {user.apellido}</li>
+                    ))}
+                </ul>
+            </p>
+            <p> Bateadores: 
+            <ul>
+                    {bateadores.map(user => (
+                        <li key={user.username}>{user.nombre} {user.apellido}</li>
+                    ))}
+                </ul>
+            </p><p> Buscador:
+                </p>
+            
+            {buscador && <p> {buscador.nombre} {buscador.apellido}</p> }
             <h4>Zonas Prohibidas</h4>
         </div>
     )
