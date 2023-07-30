52ca547fad0f8c3e2402745e7a63d6b9e19f98b0
LauMFlores <mlaumflores@gmail.com>
2023-07-30 19:11:23 -0300

    intento solucionar error build, borro salon personal viejo

--- app/salon-personal/page.tsx
+++ app/salon-personal/page.tsx
@@ -1,38 +1,16 @@
 'use client';
+import React, { useState } from "react";
 import Layout from "../layout2";
-import data from "../data_usuarios.json";
 
 
+export default function salonPersonal() {
+    const pageTitle = "Campus Mágico de Slytherin";
 
-  export default function nuevosIngresos (usuario:number){
-
-    const pageTitle = 'Salón Personal';
-
-    // const alumno = data.find(user => user.username === usuario);
 
     return (
-      <Layout pageTitle={pageTitle}>
-          <main className="campus"> 
-          <h1> BIENVENIDO ! </h1>
-          <div style={{backgroundColor: 'white'}}>
-          <div style={{backgroundColor: 'var(--verde1)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--verde2)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--verde3)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--verde4)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--verde5)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--verde6)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--verde7)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--gris1)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--gris2)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--gris3)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--gris4)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--gris5)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--gris6-transparente-menu)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--negro)'}}> COLOR</div>
-          <div style={{backgroundColor: 'var(--blanco)'}}> COLOR</div>
-          </div>
-          </main>
-      </Layout>
-  )
+        <Layout pageTitle={pageTitle}>
+        <main className="campus">
+        </main>
+    </Layout>
+    );
   }
-
